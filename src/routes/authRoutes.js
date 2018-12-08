e2c15e3076035f99feb35b7dd7a6b003a42d5846
Romain <rmtestard@gmail.com>
2018-12-08 14:04:46 +0100

    creating a user in mongoDB

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,10 +8,30 @@ function router() {
   authRouter.route('/signUp').post((req, res) => {
     debug(req.body);
     //  create user
-    //  log in, thanks to localstrategy
-    req.login(req.body, () => {
-      res.redirect('/auth/profile');
-    });
+    const { username, password } = req.body;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function addUser() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug('Connect correctly to server');
+
+        const db = client.db(dbName);
+
+        const col = db.collection('users');
+        const user = { username, password };
+        const results = await col.insertOne(user);
+        debug(results);
+        //  log in, thanks to localstrategy
+        req.login(results.ops[0], () => {
+          res.redirect('/auth/profile');
+        });
+      } catch (err) {
+        debug(err);
+      }
+    }());
   });
   authRouter.route('/profile').get((req, res) => {
     res.json(req.user);
