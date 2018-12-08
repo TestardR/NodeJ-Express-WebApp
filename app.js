be1beac6891eeeeb4c2c3a53f9b425ba81e46dd5
Romain <rmtestard@gmail.com>
2018-12-08 13:29:50 +0100

    setting up Passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const path = require('path');
 // const sql = require('mssql');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -40,6 +43,12 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 // will pull out our post request and add it to our body
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// session takes secret when it build the cooky
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use(
   '/css',
